8208995c2c77b0605362cf98c4b24c03debfca7a
foxfire <foxfire256@gmail.com>
2019-11-10 10:21:50 -0500

    Can handle input and draw a square

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace csFallingBlocks
 {
-	partial class Form1
+	partial class MainForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,10 +28,25 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(496, 473);
+			this.DoubleBuffered = true;
+			this.ForeColor = System.Drawing.Color.White;
+			this.KeyPreview = true;
+			this.Name = "MainForm";
+			this.Text = "csFallingBlocks";
+			this.ResizeEnd += new System.EventHandler(this.MainForm_ResizeEnd);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainForm_KeyPress);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
--- MainForm.cs
+++ MainForm.cs
@@ -3,18 +3,68 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.Drawing.Drawing2D;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace csFallingBlocks
 {
-	public partial class Form1 : Form
+	public partial class MainForm : Form
 	{
-		public Form1()
+		private int squareSize;
+		
+		public MainForm()
 		{
 			InitializeComponent();
+
+			squareSize = 16;
+		}
+
+		private void MainForm_Paint(object sender, PaintEventArgs e)
+		{
+			DrawSquare(5, 5, 0, e);
+		}
+
+		private void MainForm_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			
+		}
+
+		private void MainForm_ResizeEnd(object sender, EventArgs e)
+		{
+
+		}
+
+		private void DrawSquare(int x, int y, int type, PaintEventArgs e)
+		{
+			// Create a path that consists of a single ellipse.
+			GraphicsPath path = new GraphicsPath();
+
+			Rectangle rect = new Rectangle(x, y, squareSize, squareSize);
+
+			path.AddRectangle(rect);
+
+			// Use the path to construct a brush.
+			PathGradientBrush pthGrBrush = new PathGradientBrush(path);
+
+			// Set the color at the center of the path to blue.
+			pthGrBrush.CenterColor = Color.FromArgb(255, 0, 0, 255);
+
+			// Set the color along the entire boundary 
+			// of the path to aqua.
+			Color[] colors = { Color.FromArgb(255, 0, 255, 255) };
+			pthGrBrush.SurroundColors = colors;
+
+			e.Graphics.FillRectangle(pthGrBrush, rect);
+		}
+
+		private void MainForm_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.KeyCode == Keys.Escape)
+			{
+				this.Close();
+			}
 		}
 	}
 }
